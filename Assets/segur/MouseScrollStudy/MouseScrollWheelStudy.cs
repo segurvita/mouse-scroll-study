112ee5b337668b75fe02d6fad19c52faba99d468
segur <segur.opus@gmail.com>
2020-10-06 12:20:32 +0900

    Add MouseScrollWheelStudy

---
+++ Assets/segur/MouseScrollStudy/MouseScrollWheelStudy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseScrollWheelStudy : MonoBehaviour
+{
+    /// <summary>テキストボックス</summary>
+    public Text uiCurrentValue;
+    
+    /// <summary>テキストボックス</summary>
+    public Text uiTextSum;
+
+    /// <summary>スライダー</summary>
+    public Slider uiSlider;
+
+    /// <summary>合計値</summary>
+    private float _sum = 0;
+
+    /// <summary>
+    /// 毎フレーム呼ばれる関数
+    /// </summary>
+    void Update()
+    {
+        // 現在の数値を取得する。
+        float currentValue = Input.GetAxis("Mouse ScrollWheel");
+
+        if (currentValue == 0.0)
+        {
+            return;
+        }
+        
+        // 合計する
+        _sum += currentValue;
+
+        // 入力値を表示する。
+        uiCurrentValue.text = currentValue.ToString();
+        
+        // 合計値を表示する。
+        uiTextSum.text = _sum.ToString();
+        uiSlider.value = _sum;
+    }
+}
